64724068bdab670ee02c2a4aa01f56aeb0e16efe
anapothos <anapothos@gmail.com>
2019-10-02 10:10:24 -0700

    added functions

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+import { compareNumbers } from './test/compare-numbers';
+
 //iniliaze dom elements
 
 const goButton = document.getElementById ('button');
 const amountLevel = document.getElementById ('amount');
-let amount = 4; 
+let highLow = document.getElementById ('level')
+let successLevel = document.getElementById ('success')
+
+const amount = 4; 
 
 goButton.addEventListener ('click', () => {
-    amount = amount + 4; 
-
+    
+    
+    amountLevel.textContent = amount;
+    successLevel = 
+    
 });
