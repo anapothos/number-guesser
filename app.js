aa500010661f040f8c458c3776b135d5b99b2e58
anapothos <anapothos@gmail.com>
2019-10-09 12:59:25 -0700

    refined code

--- app.js
+++ app.js
@@ -2,25 +2,61 @@ import { compareNumbers } from './compare-numbers.js';
 
 //iniliaze dom elements
 
-
-const numberOfTriesLeft = document.getElementById ('tries-remaining');
-const highOrLow = document.getElementById ('high-low');
+const numberOfTriesRemaining = document.getElementById ('tries-remaining');
+const userInput = document.getElementById ('guess-input-field');
 const guessButton = document.getElementById ('guessbutton');
 const userResults = document.getElementById ('win-lose');
+const previousGuessField = document.getElementById ('results');
 
 //set initial state
-const targetNumber = 4; 
+const correctNumber = Math.floor(4); 
+let numberOfTries = 4;
 
-goButton.addEventListener ('click', () => {
+userResults.textContent = `Tries Remaining: ${numberOfTries}`;
+
+guessButton.addEventListener ('click', () => {
     let userGuess = Number(userInput.value);
-    let comparisonResult = compareNumbers(userGuess, targetNumber);
+    updatePreviousGuess(userGuess);
+    let comparisonResult = compareNumbers(userGuess, correctNumber);
     guessingGame(comparisonResult);
-    
 });
 
+const updatePreviousGuess = userGuess => {
+    previousGuessField.textContent = `Your guess was ${userGuess}`;
+};
 
 const guessingGame = comparisonResult => {
-    if (comparisonResult < 0) {
-        update;
+    if (comparisonResult < correctNumber) {
+        updateLives();
+        updateTooLow();
+    } else if (comparisonResult > correctNumber) {
+        updateLives(); 
+        updateTooHigh();
+    } else if (comparisonResult === correctNumber) {
+        winner();
+    
+    } else if (numberOfTries === 0) {
+        loser();
     }
+};
+
+const updateLives = () => {
+    numberOfTries;
+    previousGuessField.textContent = `Guesses Remaining: ${numberOfTriesRemaining}`;
+}; 
+
+const updateTooLow = () => {
+    previousGuessField.textContent = 'Guess was 2 low';
+};
+
+const updateTooHigh = () => {
+    userResults.textContent = 'Guess was 2 high';
+};
+
+const loser = () => { 
+    userResults.textContent = 'All 4 lives gone! You lose!';
+};
+
+const winner = () => {
+    userResults.textContent = 'You Win!';
 };
